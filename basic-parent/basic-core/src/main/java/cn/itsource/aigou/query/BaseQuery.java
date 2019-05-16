55450006638117c530b16e405d5f86f326a5b816
409694982 <50447803+409694982@users.noreply.github.com>
2019-05-16 23:13:53 +0800

    品牌模块的增删查

---
+++ basic-parent/basic-core/src/main/java/cn/itsource/aigou/query/BaseQuery.java
@@ -0,0 +1,39 @@
+package cn.itsource.aigou.query;
+
+/**
+ * @author zt
+ * @version V1.0
+ * @className BaseQuery
+ * @description Query的顶级父类
+ * @date 2019/5/16 19:09
+ */
+public class BaseQuery {
+
+    private Long page;
+    private Long size;
+    private String keyword;
+
+    public Long getPage() {
+        return page;
+    }
+
+    public void setPage(Long page) {
+        this.page = page;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+}
